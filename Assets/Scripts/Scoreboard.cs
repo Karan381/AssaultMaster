41eb2a71802d7dd994b137bf96ad78b437aff32d
Karan Gupta <kg042632@gmail.com>
2022-02-06 17:42:49 +0530

    scoreboard display

--- Assets/Scripts/Scoreboard.cs
+++ Assets/Scripts/Scoreboard.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Scoreboard : MonoBehaviour
 {
     // Start is called before the first frame update
     int score = 0;
-  
+    TMP_Text scoreText;
+
+    private void Start()
+    {
+        scoreText = GetComponent<TMP_Text>();
+        scoreText.text = "0";
+    }
+
     public void IncreaseScore(int scoreToIncrease)
     {
         score += scoreToIncrease;
-        Debug.Log(score);
+        scoreText.text = score.ToString();
     }
 }
