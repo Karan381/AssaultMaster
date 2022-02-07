dda86462f211a22b81a9953981341a151da5a197
Karan Gupta <kg042632@gmail.com>
2022-02-07 17:26:39 +0530

    hit vfx

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,8 +6,10 @@ public class Enemy : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] GameObject enemydedVFX;
+    [SerializeField] GameObject hitVFX;
     [SerializeField] Transform parent;
     [SerializeField] int EnemyScore;
+    [SerializeField] int enemyHitCount = 0;
 
     Scoreboard scoreBoard;
 
@@ -17,20 +19,25 @@ public class Enemy : MonoBehaviour
     }
     private void OnParticleCollision(GameObject other)
     {
-        KillEnemy();
         ProcessHit();
-
+        if (enemyHitCount < 1)
+        {
+            KillEnemy();
+        }
     }
 
     private void KillEnemy()
     {
-        GameObject vfx = Instantiate(enemydedVFX, transform.position, Quaternion.identity);
-        vfx.transform.parent = parent;
-        Destroy(gameObject);
+            GameObject vfx = Instantiate(enemydedVFX, transform.position, Quaternion.identity);
+            vfx.transform.parent = parent;
+            Destroy(gameObject);
     }
 
     private void ProcessHit()
     {
+        GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;
         scoreBoard.IncreaseScore(EnemyScore);
+        enemyHitCount--;
     }
 }
