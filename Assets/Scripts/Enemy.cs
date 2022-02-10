fbe1e9a946881051077929c52acbe2eabc4d0567
Karan Gupta <kg042632@gmail.com>
2022-02-10 13:26:44 +0530

    enemy setup

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,16 +7,26 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField] GameObject enemydedVFX;
     [SerializeField] GameObject hitVFX;
-    [SerializeField] Transform parent;
     [SerializeField] int EnemyScore;
     [SerializeField] int enemyHitCount = 0;
 
     Scoreboard scoreBoard;
+    GameObject parentGameObject;
 
     private void Start()
     {
         scoreBoard = FindObjectOfType<Scoreboard>();
+        AddRigidBody();
+        parentGameObject = GameObject.FindWithTag("SpawnAtRuntime");
+        
     }
+
+    private void AddRigidBody()
+    {
+        Rigidbody rb = gameObject.AddComponent<Rigidbody>();
+        rb.useGravity = false;
+    }
+
     private void OnParticleCollision(GameObject other)
     {
         ProcessHit();
@@ -29,14 +39,14 @@ public class Enemy : MonoBehaviour
     private void KillEnemy()
     {
             GameObject vfx = Instantiate(enemydedVFX, transform.position, Quaternion.identity);
-            vfx.transform.parent = parent;
+        vfx.transform.parent = parentGameObject.transform;
             Destroy(gameObject);
     }
 
     private void ProcessHit()
     {
         GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
-        vfx.transform.parent = parent;
+        vfx.transform.parent = parentGameObject.transform;
         scoreBoard.IncreaseScore(EnemyScore);
         enemyHitCount--;
     }
